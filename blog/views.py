5e5fe73d2856bbff9e5974839a8b039438ccbb2c
Your Name <you@example.com>
2023-08-07 02:44:15 +0400

    add HTML in 'post_list'

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 from django.template import Context, loader
 from django.views.generic import ListView, DetailView
-from django.views.generic import ListView
 from .models import Post
 
 class PostList(ListView):
