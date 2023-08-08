6d16de2c7b47e1bb1bdbe54ed2c85b9d4fc03981
Your Name <you@example.com>
2023-08-08 18:41:11 +0400

    add postts

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,16 @@ from django.http  import HttpResponse
 from django.template import Context, loader
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.utils import timezone
 
 class PostList(ListView):
     model = Post
     template_name = 'post_list.html'
     context_object_name = 'post'
 
-#def post_list(request):
-    #return render(request, 'blog/post_list.html',{})
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 #def post_list (request):
     #return HttpResponse('blog/post_list.html')
